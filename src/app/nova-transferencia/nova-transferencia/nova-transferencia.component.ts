c105ed189f765e18496414f0d55446e96f1044d0
Pablo Dias <pablo.dias.dev@gmail.com>
2022-07-13 11:59:48 -0300

    Transfer component

---
+++ src/app/nova-transferencia/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  transferir() {
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
